ee1acb81a91f4deddd6b4854a0598b0006c08918
Avataar120 <dric.vivien@gmail.com>
2025-06-01 00:40:41 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,136 @@
+#include <Arduino.h>
+#include "esp_timer.h"
+
+#define ADC_PIN 4 // GPIO4 = ADC1_CHANNEL_3 sur l'ESP32-S3
+
+uint32_t start = 0;
+uint32_t stop = 0;
+String s;
+bool New = true;
+bool OneOutOfTwo = true;
+
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000); // Laisse le temps au port série de s'initialiser
+
+  analogReadResolution(12);      // Résolution de 12 bits (0 - 4095)
+  analogSetAttenuation(ADC_0db); // Échelle de tension jusqu’à ~3.3V
+  Serial.println("Initialisation terminée. Lecture ADC en cours...");
+
+  start = esp_timer_get_time();
+}
+
+String GetChar(String s)
+{
+  if (s == "1010")
+    return "0";
+  if (s == "1011")
+    return "1";
+  if (s == "1100")
+    return "2";
+  if (s == "0011")
+    return "3";
+  if (s == "1101")
+    return "4";
+  if (s == "0101")
+    return "5";
+  if (s == "0110")
+    return "6";
+  if (s == "0111")
+    return "7";
+  if (s == "1110")
+    return "8";
+  if (s == "1001")
+    return "9";
+  return "!";
+}
+
+void Decode(String s)
+{
+  Serial.println(s);
+  if (s.length() == 58)
+  {
+
+    String Preamble = s.substring(0, 2);
+    String Sync1 = s.substring(2, 6);
+    String Sync2 = s.substring(6, 10);
+
+    String ID = "";
+    for (int i = 0; i < 6; i++)
+    {
+      ID += GetChar(s.substring(10 + i * 4, 10 + 4 + i * 4));
+    }
+
+    int Pressure = std::stoi(s.substring(34, 46).c_str(), nullptr, 2);
+
+    String Batt = s.substring(46, 50);
+    if (Batt = "0000")
+      Batt = "OK";
+    else if (Batt = "0010")
+      Batt = "Low";
+    else if (Batt = "0001")
+      Batt = "Critical";
+
+    int ChecksumCalc = 0;
+    for (int i = 0; i < 12; i++)
+    {
+      int v = std::stoi(s.substring(2 + i * 4, 2 + 4 + i * 4).c_str(), nullptr, 2);
+      ChecksumCalc += v;
+    }
+
+    int ChesumMsg = std::stoi(s.substring(50, 58).c_str(), nullptr, 2);
+
+    Serial.printf("Preamble : %s\n", Preamble.c_str());
+    Serial.printf("Sync1 : %s\n", Sync1.c_str());
+    Serial.printf("Sync2 : %s\n", Sync2.c_str());
+    Serial.printf("ID : %s\n", ID.c_str());
+    Serial.printf("Pressure : %d PSI - %.2f bars\n", Pressure * 2, Pressure * 2 / 14.504);
+    Serial.printf("Battery : %s\n", Batt);
+    Serial.printf("Checksum calc : %d - Checksum message : %d - ", ChecksumCalc, ChesumMsg);
+    if (ChecksumCalc == ChesumMsg)
+      Serial.printf("OK\n\n");
+    else
+      Serial.printf("NOK\n\n");
+  }
+}
+
+void loop()
+{
+  int adcValue = analogReadMilliVolts(ADC_PIN);
+  if (adcValue > 80)
+  {
+    if (New)
+    {
+      start = esp_timer_get_time();
+      New = false;
+    }
+
+    uint32_t delta = esp_timer_get_time() - start;
+
+    if ((delta > 1700) && (delta < 2300))
+    {
+      s += "0";
+      start = esp_timer_get_time();
+    }
+    if ((delta > 2700) && (delta < 3500))
+    {
+      s += "1";
+      start = esp_timer_get_time();
+    }
+    if (delta > 6000)
+    {
+
+      if (OneOutOfTwo)
+      {
+        Decode(s);
+        OneOutOfTwo = false;
+      }
+      else
+        OneOutOfTwo = true;
+
+      s = "";
+      New = true;
+    }
+  }
+}
